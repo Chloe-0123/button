9510ace765197f4c909f375f54f2d67c4a5166de
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2024-03-01 23:42:01 +0900

    upload button

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { useAnimate } from 'framer-motion'
+
+function App() {
+
+  const [scope, animate] = useAnimate()
+
+  const mouseEnterAction = () => {
+    
+    animate([
+      [".animated", {y:-45}, { duration: 0.4}],
+      [".animated", {y: 0}, {duration: 0.0001 }]
+    ])
+  }
+  return (
+    <>
+      <div className='button-container' ref={scope}> 
+        <button onMouseEnter={mouseEnterAction}>
+          <a href="#">
+            <div className="back">
+              <h2 className='animated'>Discover more of our work</h2>
+            </div>
+          </a>
+        </button>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
